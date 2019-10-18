51a498e1cce52ec99e7a013cc76c27eb2a097467
dupuyr <dupuyr@tcd.ie>
2019-10-18 13:28:31 +0100

    revised tree tests

--- src/LCATreeTest.java
+++ src/LCATreeTest.java
@@ -25,45 +25,5 @@ class LCATreeTest {
 	    assertEquals(two.nodeNumber, a.nodeNumber);
 	}
 	
-	// LCA of two nodes in a directed graph
-	@Test
-	void directedGraphTest()
-	{
-		LCA t = new LCA();
-		Node r = new Node(1);
-	    Node two = new Node(2);
-	    Node three = new Node(3);
-	    Node four = new Node(4);
-	    Node five = new Node(5);
-	    Node six = new Node(6);
-	    Node.haveKids(r,two);
-	    Node.haveKids(two,three);
-	    Node.haveKids(three,five);
-	    Node.haveKids(four,two);
-	    Node.haveKids(five,six);
-	    
-	    Node a = t.findLCA(r, three, six);
-	    assertEquals(two.nodeNumber, a.nodeNumber);
-	}
 	
-	// LCA of two nodes in an undirected graph
-	@Test
-	void undirectedGraphTest()
-	{
-		LCA t = new LCA();
-		Node r = new Node(1);
-	    Node two = new Node(2);
-	    Node three = new Node(3);
-	    Node four = new Node(4);
-	    Node five = new Node(5);
-	    Node.haveKids(r,two);
-	    Node.haveKids(r,three);
-	    Node.haveKids(two,three);
-	    Node.haveKids(three,four);
-	    Node.haveKids(four,five);
-	    
-	    Node a = t.findLCA(r, three, four);
-	    assertEquals(two.nodeNumber, a.nodeNumber);
-	}
-
 }
